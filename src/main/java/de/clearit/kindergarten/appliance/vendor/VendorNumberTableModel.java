376714e221c35d71be8a23d0339c6b5f29855640
Kenan Horoz <kenan.horoz@clear-it.de>
2018-02-01 11:15:11 +0100

    Vendor change (#6)
    * Test
    
    * Test
    
    * new layout, addBtn and deleteBtn
    
    * small layout changes
    
    * deleted debugging sysout

---
+++ src/main/java/de/clearit/kindergarten/appliance/vendor/VendorNumberTableModel.java
@@ -0,0 +1,40 @@
+package de.clearit.kindergarten.appliance.vendor;
+import javax.swing.ListModel;
+import com.jgoodies.application.Application;
+import com.jgoodies.application.ResourceMap;
+import com.jgoodies.binding.adapter.AbstractTableAdapter;
+import de.clearit.kindergarten.domain.VendorBean;
+
+/**
+ * 
+ * @author Kenan.Horoz
+ *TableModle for the table in VendorEditorView 
+ *containing VendorNumbers
+ */
+  public class VendorNumberTableModel extends AbstractTableAdapter<String> {
+
+  private static final long serialVersionUID = 1L;
+  private static final ResourceMap RESOURCES = Application.getResourceMap(VendorHomeView.class);
+
+  public VendorNumberTableModel(ListModel<?> listModel) {
+    super(listModel, getColumnNames());
+  }
+
+  private static String[] getColumnNames() {
+    return new String[] { "zugewiesene Verkäufernummern" };
+  }
+  
+  @Override
+  public Object getValueAt(int rowIndex, int columnIndex) {
+    String vendorNumber = getRow(rowIndex);
+    switch (columnIndex) {
+    case 0:
+      return vendorNumber;
+    default:
+      throw new IllegalStateException("Can't handle column index " + columnIndex);
+    }
+  }
+  
+  
+
+}
