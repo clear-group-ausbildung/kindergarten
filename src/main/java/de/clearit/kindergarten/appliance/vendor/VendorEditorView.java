2c53cc0014e2a8644e87b383207ec654d122fb39
TheDonDope <idontreallyneedthese@gmail.com>
2018-03-01 18:40:02 +0100

    fix(vendors): transfer focus input field

--- src/main/java/de/clearit/kindergarten/appliance/vendor/VendorEditorView.java
+++ src/main/java/de/clearit/kindergarten/appliance/vendor/VendorEditorView.java
@@ -69,6 +69,9 @@ public final class VendorEditorView extends AbstractView {
         vendorNumberField.requestFocusInWindow();
       }
     });
+    addVendorNumberButton.addActionListener(actionEvent -> {
+      vendorNumberField.requestFocusInWindow();
+    });
   }
 
   @Override
