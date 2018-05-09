a4c416ebd3d52d7b395ee0ed22b49098253b230c
Christian Dobert <idontreallyneedthese@gmail.com>
2018-05-09 20:44:56 +0200

    feat(vendors): Druckfunktion wurde implementiert. (#49)

--- src/main/java/de/clearit/kindergarten/domain/export/ExportReceipt.java
+++ src/main/java/de/clearit/kindergarten/domain/export/ExportReceipt.java
@@ -61,7 +61,7 @@ public class ExportReceipt {
 
   /**
    * Creates an receipt in excel for the given vendor.
-   * 
+   *
    * @param pVendor
    *          {@link VendorBean}
    */
@@ -215,7 +215,7 @@ public class ExportReceipt {
     return null;
   }
 
-  private String getDateiname(PayoffDataReceipt pPayoffData) {
+  public String getDateiname(PayoffDataReceipt pPayoffData) {
     String folder = System.getProperty("user.home") + "/Desktop/Basar Abrechnungen";
     StringBuilder dateiName = new StringBuilder();
     if (!Files.isDirectory(Paths.get(folder))) {
