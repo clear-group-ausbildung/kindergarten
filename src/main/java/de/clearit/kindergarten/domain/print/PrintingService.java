7e261348ce70bf64454b617edbcc2fc2e4e074d6
TheDonDope <idontreallyneedthese@gmail.com>
2018-09-26 10:38:12 +0200

    refac(print): clean up print service
    - Use correct unicode for umlauts
    - Remove dead code

--- src/main/java/de/clearit/kindergarten/domain/print/PrintingService.java
+++ src/main/java/de/clearit/kindergarten/domain/print/PrintingService.java
@@ -41,7 +41,7 @@ public class PrintingService {
     List<VendorBean> vendorList = VendorService.getInstance().getAll();
     allPDFFiles.clear();
     vendorList.forEach(PrintingService::createFile);
-    //Creates one File for printing
+    // Creates one File for printing
     sendToPrinter();
   }
 
@@ -49,58 +49,63 @@ public class PrintingService {
     PayoffDataReceipt payoffData = ExportDataService.getPayoffDataForVendor(vendor);
     File pdfToPrint = new File(ExportReceipt.getInstance().getDateiname(payoffData).replace(".xlsx", ".pdf"));
     allPDFFiles.add(pdfToPrint);
-   }
-  
+  }
+
   @SuppressWarnings("deprecation")
   private static void sendToPrinter() throws IOException {
-	  final String path = System.getProperty("user.home") + "/Desktop/Basar Abrechnungen/Gesamt Abrechnung.pdf";
-	  PDFMergerUtility pdfMerger = new PDFMergerUtility();
-	  //If u remove this, u got a Message in the console:
-	  //INFO To get higher rendering speed on JDK8 or later,
-	  //INFO use the option -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider
-	  //INFO or call System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider") 
-	  System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider"); 
-	  if(allPDFFiles.size() != 0) {
-		  for (File file : allPDFFiles) {
-			pdfMerger.addSource(file);
-		  }
-		  pdfMerger.setDestinationFileName(path);
-		  pdfMerger.mergeDocuments();
-		  
-		  File pdfToPrint = new File(pdfMerger.getDestinationFileName());
-		  
-		  DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE;
-		  PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-		  javax.print.PrintService[] services = PrintServiceLookup.lookupPrintServices(flavor, attributes);
-		  javax.print.PrintService defaultService = PrintServiceLookup.lookupDefaultPrintService();
+    final String path = System.getProperty("user.home") + "/Desktop/Basar Abrechnungen/Gesamt Abrechnung.pdf";
+    PDFMergerUtility pdfMerger = new PDFMergerUtility();
+    // If u remove this, u got a Message in the console:
+    // INFO To get higher rendering speed on JDK8 or later,
+    // INFO use the option -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider
+    // INFO or call System.setProperty("sun.java2d.cmm",
+    // "sun.java2d.cmm.kcms.KcmsServiceProvider")
+    System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider");
+    if (allPDFFiles.size() != 0) {
+      for (File file : allPDFFiles) {
+        pdfMerger.addSource(file);
+      }
+      pdfMerger.setDestinationFileName(path);
+      pdfMerger.mergeDocuments();
 
-		  if (defaultService == null) {
-			  LOGGER.debug("Error - No default print service found");
-		  }else {
-			  int reply = JOptionPane.showConfirmDialog(null, "Sie sind dabei ALLE Abrechnungen zu drucken. Möchten Sie fortfahren?" , "Alle Abrechnunge Drucken",JOptionPane.YES_NO_OPTION);
-			  if(reply == JOptionPane.YES_OPTION) {
-				  javax.print.PrintService service = ServiceUI.printDialog(null, 200, 200, services, defaultService, flavor, attributes);
-				  if(service != null) {
-			  		DocPrintJob docPrintJob = service.createPrintJob();
-			  		try {
-			          PDDocument pdDocument = PDDocument.load(pdfToPrint);	
-			          // Drucken als Pageable
-			          PDFPageable pdfPageable = new PDFPageable(pdDocument);
-			          SimpleDoc docToPrint = new SimpleDoc(pdfPageable, flavor, null);
-			          docPrintJob.print(docToPrint, null);
-			        }catch(PrintException e) {
-			        	LOGGER.debug("Error - Unable to print");
-			        	LOGGER.error(e.getMessage());
-			        }catch (IOException e) {
-			          LOGGER.error(e.getMessage());
-			        }
-				  }
-			  }else {
-				  JOptionPane.showMessageDialog(new JFrame(), "Gesamtabrechnung wurde erstellt!\n" + path);
-			  }
-		  }
-	  }else {
-		  JOptionPane.showMessageDialog(new JFrame(), "Keine Verkäufer erfasst Drucken nicht möglich!", "Fehler", JOptionPane.ERROR_MESSAGE);
-	  }
+      File pdfToPrint = new File(pdfMerger.getDestinationFileName());
+
+      DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE;
+      PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
+      javax.print.PrintService[] services = PrintServiceLookup.lookupPrintServices(flavor, attributes);
+      javax.print.PrintService defaultService = PrintServiceLookup.lookupDefaultPrintService();
+
+      if (defaultService == null) {
+        LOGGER.debug("Error - No default print service found");
+      } else {
+        int reply = JOptionPane.showConfirmDialog(null,
+            "Sie sind dabei ALLE Abrechnungen zu drucken. M\u00f6chten Sie fortfahren?", "Alle Abrechnunge Drucken",
+            JOptionPane.YES_NO_OPTION);
+        if (reply == JOptionPane.YES_OPTION) {
+          javax.print.PrintService service = ServiceUI.printDialog(null, 200, 200, services, defaultService, flavor,
+              attributes);
+          if (service != null) {
+            DocPrintJob docPrintJob = service.createPrintJob();
+            try {
+              PDDocument pdDocument = PDDocument.load(pdfToPrint);
+              // Drucken als Pageable
+              PDFPageable pdfPageable = new PDFPageable(pdDocument);
+              SimpleDoc docToPrint = new SimpleDoc(pdfPageable, flavor, null);
+              docPrintJob.print(docToPrint, null);
+            } catch (PrintException e) {
+              LOGGER.debug("Error - Unable to print");
+              LOGGER.error(e.getMessage());
+            } catch (IOException e) {
+              LOGGER.error(e.getMessage());
+            }
+          }
+        } else {
+          JOptionPane.showMessageDialog(new JFrame(), "Gesamtabrechnung wurde erstellt!\n" + path);
+        }
+      }
+    } else {
+      JOptionPane.showMessageDialog(new JFrame(), "Keine Verk\u00e4ufer erfasst Drucken nicht m\u00f6glich!", "Fehler",
+          JOptionPane.ERROR_MESSAGE);
+    }
   }
 }
